031944ba0c6989425fe5ede70c037e6a140e1837
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-13 18:35:14 -0500

    Create Beta.h

---
+++ Semana5/Ejercicio4/Beta.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Entidad.h"
+
+class Beta : public Entidad
+{
+public:
+	Beta()
+	{
+		ID = 2;
+		ancho = 11; alto = 3;
+		x = (ANCHO / 2) - (ancho / 2);     y = 0;
+		color = ConsoleColor::Yellow;
+	}
+	void Mostrar()
+	{
+		Console::ForegroundColor = color;
+		Console::SetCursorPosition(x, y + 0); cout << "   .---.   ";
+		Console::SetCursorPosition(x, y + 1); cout << " _/__~0_\\_ ";
+		Console::SetCursorPosition(x, y + 2); cout << "(_________)";
+	}
+};
