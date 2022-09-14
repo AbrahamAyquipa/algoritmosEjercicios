7168c45ce539d82241bbee22ad854ec1768c5882
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-14 18:19:19 -0500

    Update Entidad.h

--- Semana5/Ejercicio4/Entidad.h
+++ Semana5/Ejercicio4/Entidad.h
@@ -9,8 +9,7 @@ using namespace System;
 #define ANCHO 80
 #define ALTO 20
 
-class Entidad
-{
+class Entidad {
 protected:
 	int ID;
 	int x, y;
@@ -18,60 +17,25 @@ protected:
 	ConsoleColor color;
 
 public:
-	int GetX()
-	{
-		return x;
-	}
-	int GetY()
-	{
-		return y;
-	}
-	int GetAncho()
-	{
-		return ancho;
-	}
-	int GetAlto()
-	{
-		return alto;
-	}
-	int GetID()
-	{
-		return ID;
-	}
+	int GetX() { return x; }
+	int GetY() { return y; }
+	int GetAncho() { return ancho; }
+	int GetAlto() { return alto; }
+	int GetID() { return ID; }
 
-	void SetX(int x)
-	{
-		this->x = x;
-	}
-	void SetY(int y)
-	{
-		this->y = y;
-	}
-	void SetAncho(int ancho)
-	{
-		this->ancho = ancho;
-	}
-	void SetAlto(int alto)
-	{
-		this->alto = alto;
-	}
+	void SetX(int x) { this->x = x; }
+	void SetY(int y) {this->y = y; }
+	void SetAncho(int ancho) { this->ancho = ancho; }
+	void SetAlto(int alto) { this->alto = alto; }
+	void SetColor(ConsoleColor color) { this->color = color; }
 
-	void SetColor(ConsoleColor color)
-	{
-		this->color = color;
-	}
-
-	void Borrar()
-	{
+	void Borrar() {
 		for (int i = 0; i < alto; i++)
-			for (int j = 0; j < ancho; j++)
-			{
+			for (int j = 0; j < ancho; j++) {
 				Console::SetCursorPosition(x + j, y + i);
 				cout << " ";
 			}
 	}
-	void Mover()
-	{
-		y++;
-	}
+
+	void Mover() { y++; }
 };
