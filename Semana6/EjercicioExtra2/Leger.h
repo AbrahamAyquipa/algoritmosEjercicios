38d50315da26ffed29db1097f2c7a228303c5d01
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-22 17:48:48 -0500

    Create Leger.h

---
+++ Semana6/EjercicioExtra2/Leger.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Vehiculo.h"
+
+class Leger : public Vehiculo
+{
+
+public:
+	Leger(double px, int py);
+	~Leger();
+	double get_x();
+};
+//IMPLEMENTACION
+Leger::Leger(double px, int py) :Vehiculo(px, py)
+{
+
+}
+
+Leger::~Leger()
+{}
+
+double Leger::get_x()
+{
+	return this->x;
+}
