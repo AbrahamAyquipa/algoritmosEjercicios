45fb58f0dddb29c0381916df40bdcbb78c346d56
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-26 16:12:34 -0500

    Create main.cpp

---
+++ Semana7/EjercicioExtra/main.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include <ctime>
+#include <conio.h>
+#include "ArrPlanV.h"
+
+int main() {
+	srand(time(NULL));
+	ArrPlanV* arr = new ArrPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	arr->agregarPlanV();
+	
+	cout << "\nReporte Vacaciones en rango de fechas";
+	cout << "\n----------------------------------------\n";
+	arr->reporte1(1990, 2021);
+	getch(); system("cls");
+	cout << "\nReporte Vacaciones Hotel Westin";
+	cout << "\n---------------------------------\n";
+	arr->reporte2("Westin");
+	getch(); system("cls");
+	cout << "\nReporte Plan de Vacaciones";
+	cout << "\n---------------------------\n";
+	arr->reporte3();
+	getch(); system("cls");
+	arr->reporte31();
+	cin.get();
+	cin.ignore();
+	return 0;
+}
