15ce0d2ceeec5de11dc3052de4d5e929f5f30ab1
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-26 16:13:10 -0500

    Create Date.h

---
+++ Semana7/EjercicioExtra/Date.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Date {
+private :
+	int d, m, a;
+public: 
+	Date() {
+		d = rand() % 31 + 1;
+		m = rand() % 12 + 1;
+		a = 1950 + rand() % 72;
+	}
+	~Date() {}
+	int getD() { return d; }
+	int getM() { return m; }
+	int getA() { return a; }
+	string getFechaCompleta() {
+		//dd//mm//aa
+		string aux;
+		aux.append(to_string(d));
+		aux.append("/");
+		aux.append(to_string(m));
+		aux.append("/");
+		aux.append(to_string(a));
+		return aux;
+	}
+
+
+};
+
+
