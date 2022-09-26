082dcbd6a9d0272bbdaf0f2134544b497383864f
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-26 16:19:38 -0500

    Update ArrPlanV.h

--- Semana7/EjercicioExtra/ArrPlanV.h
+++ Semana7/EjercicioExtra/ArrPlanV.h
@@ -6,14 +6,18 @@ private:
 	vector< PlanVacaciones*> arr; //PlanVacaciones **arr;
 public:
 	ArrPlanV() {}
+
 	~ArrPlanV() {}
+
 	void agregarPlanV() {
 		PlanVacaciones* obj = new PlanVacaciones();
 		arr.push_back(obj); // push_back: agrega un objeto al final del vector
 	}
+
 	void borrarUltimoPlanV() {
 		arr.pop_back(); // pop_back elimina el ultimo objeto de un vector
 	}
+
 	//eliminar un plan de vacaciones en posicion
 	void eliminarPos(int pos) {
 		//arr.erase(arr.begin() + pos);
@@ -23,11 +27,11 @@ public:
 		//it=(index,obj)
 		arr.erase(it);
 	}
+
 	//-	Mostrar la lista de vacaciones en un intervalo de fecha
 	void reporte1(int a1, int a2) {
 		if (a1 <= a2) {
-			for (int i = 0; i < arr.size(); i++)
-			{
+			for (int i = 0; i < arr.size(); i++) {
 				if (arr[i]->getAnio() >= a1 && arr[i]->getAnio() <= a2) {
 					arr[i]->getInfoPlanV();
 				}
@@ -37,28 +41,27 @@ public:
 		else
 			cout << "El anio " << a1 << " Debe ser menor al " << a2 << endl;
 	}
+
 	//-	Mostrar la lista de vacaciones que tiene el mismo Hotel
 	void reporte2(string nombre) {
-		for (int i = 0; i < arr.size(); i++)
-		{
+		for (int i = 0; i < arr.size(); i++) {
 			if (arr[i]->getNombre() == nombre) {
 					cout << "\n";
 					arr[i]->getInfoPlanV();
 			}
-		
 		}
 	}
+
 	//-	Los boletos que tiene cada Plan de Vacación
 	void reporte3() {
-		for (int i = 0; i < arr.size(); i++)
-		{
+		for (int i = 0; i < arr.size(); i++) {
 			cout << "\n";
 			arr[i]->getInfoBoleto();
 		}
 	}
+
 	void reporte31() {
-		for (PlanVacaciones* p : arr)
-		{
+		for (PlanVacaciones* p : arr) {
 			cout << "\n";
 			p->getInfoBoleto();
 		}
