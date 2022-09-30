f2187b4af107ab338ffca9331457464b3e827333
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-09-29 19:22:22 -0500

    Create Source.cpp

---
+++ Semana7/EjercicioExtra3/Source.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "Controlador.h"
+
+int main() {
+    Console::SetWindowSize(120, 30);
+    Console::CursorVisible = false;
+    Controlador* controlador = new Controlador();
+    int tecla;
+
+    while (true) {
+        controlador->borrarTodo();
+        controlador->colisiones();
+        controlador->obtenerGanador();
+
+        if (_kbhit()) {
+            tecla = _getch();
+            tecla = toupper(tecla);
+            controlador->get_nave()->posicionar(tecla);
+        }
+
+        controlador->posicionarTodo();
+        controlador->dibujarTodo();
+        _sleep(100);
+    }
+}
