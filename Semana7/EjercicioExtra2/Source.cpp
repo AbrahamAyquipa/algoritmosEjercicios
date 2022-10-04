a6d1b8fbbe6420bd737ba9cb0458cf30a704f5b7
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-04 15:31:36 -0500

    Update Source.cpp

--- Semana7/EjercicioExtra2/Source.cpp
+++ Semana7/EjercicioExtra2/Source.cpp
@@ -1,23 +1,38 @@
 #include "pch.h"
 #include "Rectangulo.h"
 #include "Romboide.h"
+#include "Triangulo.h"
 
 int main() {
 	double base = 5.2;
 	double altura = 9.6;
-	Rectangulo* objRectangulo = new Rectangulo();
-	Romboide* objRomboide = new Romboide();
+	double lado1 = 2;
+	double lado2 = 6;
+	double lado3 = 56;
+
+	Rectangulo* objRectangulo = new Rectangulo(base, altura, lado1, lado2, lado3);
+	Romboide* objRomboide = new Romboide(base, altura, lado1, lado2, lado3);
+	Triangulo* objTriangulo = new Triangulo(base, altura, lado1, lado2, lado3);
+
 	cout << "Calular el area de un rectangulo: ";
-	objRectangulo->calcularArea(base, altura);
+	objRectangulo->calcularArea();
 	cout << endl;
 	cout << "Calular el perimetro de un rectangulo: ";
-	objRectangulo->calcularPerimetro(base, altura);
+	objRectangulo->calcularPerimetro();
 	cout << endl << endl;
+
 	cout << "Calular el area de un romboide: ";
-	objRomboide->calcularArea(base, altura);
+	objRomboide->calcularArea();
 	cout << endl;
 	cout << "Calular el perimetro de un romboide: ";
-	objRomboide->calcularPerimetro(base, altura);
+	objRomboide->calcularPerimetro();
 	cout << endl << endl;
+
+	cout << "Calular el area de un triangulo: ";
+	objTriangulo->calcularArea();
+	cout << endl;
+	cout << "Calular el perimetro de un triangulo: ";
+	objTriangulo->calcularPerimetro();
+	cout << endl;
 	return 0;
 }
