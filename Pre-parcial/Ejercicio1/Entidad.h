0680ff71e119508e217a45a83989c0bc37193c27
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-04 14:47:20 -0500

    Create Entidad.h

---
+++ Pre-parcial/Ejercicio1/Entidad.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+using namespace std;
+using namespace System;
+
+class Entidad {
+protected:
+	int x, y;
+	int dx, dy;
+	int w, h;
+public:
+	Entidad(int x, int y, int dx, int dy, int w, int h) {
+		this->x = x;
+		this->y = y;
+		this->dx = dx;
+		this->dy = dy;
+		this->w = w;
+		this->h = h;
+	}
+
+	~Entidad() {}
+
+	void borrar() {
+		for (int i = this->x; i < this->x + this->w; ++i) {
+			for (int j = this->y; j < this->y + this->h; ++j) {
+				System::Console::SetCursorPosition(i, j);
+				cout << ' ';
+			}
+		}
+	}
+
+	void setX(int x) { this->x = x; }
+	void setY(int y) { this->y = y; }
+
+	int getW() { return this->w; }
+	int getH() { return this->h; }
+
+	System::Drawing::Rectangle getRectangle() {
+		return System::Drawing::Rectangle(this->x, this->y, this->w, this->h);
+	}
+};
