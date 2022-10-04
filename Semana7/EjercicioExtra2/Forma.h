1b3ec573fd28433a43fa462b640f9805906daef6
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-04 15:29:25 -0500

    Update Forma.h

--- Semana7/EjercicioExtra2/Forma.h
+++ Semana7/EjercicioExtra2/Forma.h
@@ -6,10 +6,16 @@ class Forma {
 protected:
 	double base;
 	double altura;
+	double lado1;
+	double lado2;
+	double lado3;
 public:
-	Forma(double base, double altura) {
+	Forma(double base, double altura, double lado1, double lado2, double lado3) {
 		this->base = base;
 		this->altura = altura;
+		this->lado1 = lado1;
+		this->lado2 = lado2;
+		this->lado3 = lado3;
 	}
 
 	~Forma() {}
