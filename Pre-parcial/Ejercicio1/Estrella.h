9a41485d14351ef8c2df5f08100ebd0b683e4662
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-04 14:50:40 -0500

    Create Estrella.h

---
+++ Pre-parcial/Ejercicio1/Estrella.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Entidad.h"
+
+class Estrella : public Entidad {
+private:
+	bool activo;
+	int color;
+public:
+	Estrella(int width, int height) : Entidad(x, y, dx, dy, w, h) {
+		System::Random numero_aleatorio;
+
+		this->x = y = 0;
+
+		this->w = h = 1;
+
+		this->activo = true;
+
+		this->color = 12;
+	}
+
+	~Estrella() {}
+
+	void imprimir() {
+		System::Console::ForegroundColor = (ConsoleColor)this->color;
+		System::Console::SetCursorPosition(this->x, this->y); cout << "*";
+	}
+
+	void setActivo(bool estado) { this->activo = estado; }
+	bool getActivo() { return this->activo; }
+};
