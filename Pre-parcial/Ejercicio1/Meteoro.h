ffea9cd77fb0bd7ba24f1ba8afe62968db3c042c
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-04 14:48:05 -0500

    Create Meteoro.h

---
+++ Pre-parcial/Ejercicio1/Meteoro.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Entidad.h"
+
+class Meteotoro : public Entidad {
+private:
+	bool activo;
+	int color;
+	int width;
+	int height;
+public:
+	Meteotoro(int width, int height) : Entidad(x, y, dx, dy, w, h) {
+		System::Random numero_aleatorio;
+
+		this->width = width;
+		this->height = height;
+
+		this->x = y = 0;
+		this->dy = 1;
+
+		this->w = 4;
+		this->h = 1;
+
+		this->activo = true;
+
+		this->color = 1;
+	}
+
+	~Meteotoro() {}
+
+	void mover() {
+		//rebotar vaias veces
+		if (this->y + this->dy < 0 || this->y + this->h + this->dy > height) { 
+			this->dy *= -1;
+			this->color++;
+			if (color == 15) color = 1;
+		}
+		//if (this->y + this->dy < 0 || this->y + this->h + this->dy > height) this->dy += -1; rebotar una sola vez
+		this->y += this->dy;
+	}
+
+	void imprimir() {
+		System::Console::ForegroundColor = (ConsoleColor)this->color;
+		System::Console::SetCursorPosition(this->x, this->y); cout << "#__#";
+	}
+
+	void setActivo(bool estado) { this->activo = estado; }
+	bool getActivo() { return this->activo; }
+};
