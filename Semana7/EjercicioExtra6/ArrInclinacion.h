c3f70f77949eac6c6381c3b1d06af5e6680355b7
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-07 18:18:25 -0500

    Create ArrInclinacion.h

---
+++ Semana7/EjercicioExtra6/ArrInclinacion.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Inclinacion.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ArrInclinaciones {
+private:
+	vector<Inclinacion*> arr;
+	short dc = 0, ic = 0, c = 0, dl = 0, il = 0;
+public:
+	ArrInclinaciones() {}
+	~ArrInclinaciones() {}
+	void agregarInclinacion(Inclinacion* obj) {
+		arr.push_back(obj); // push_back: agrega un objeto al final del vector
+	}
+
+	void reporte() {
+		for (Inclinacion* i : arr) {
+
+			switch (i->getID()) {
+			case 1: dc++; break;
+			case 2: ic++; break;
+			case 3: c++; break;
+			case 4: dl++; break;
+			case 5: il++; break;
+			}
+		}
+		cout << "\nDER CONSERVADORA\t" << dc << " casos";
+		cout << "\nIZQ CONSERVADORA\t" << ic << " casos";
+		cout << "\nCENTRO\t\t\t" << c << " casos";
+		cout << "\nDER LIBERAL\t\t" << dl << " casos";
+		cout << "\nIZQ LIBERAL\t\t" << il << " casos";
+	}
+};
