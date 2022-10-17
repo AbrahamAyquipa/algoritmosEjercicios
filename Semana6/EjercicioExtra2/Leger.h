cb78a24baf8d5137b99e6e17184371832bf229dd
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-17 18:55:08 -0500

    Update Leger.h

--- Semana6/EjercicioExtra2/Leger.h
+++ Semana6/EjercicioExtra2/Leger.h
@@ -1,24 +1,16 @@
 #pragma once
 #include "Vehiculo.h"
 
-class Leger : public Vehiculo
-{
-
+class Leger : public Vehiculo {
 public:
 	Leger(double px, int py);
 	~Leger();
 	double get_x();
 };
+
 //IMPLEMENTACION
-Leger::Leger(double px, int py) :Vehiculo(px, py)
-{
+Leger::Leger(double px, int py) :Vehiculo(px, py) {}
 
-}
+Leger::~Leger(){}
 
-Leger::~Leger()
-{}
-
-double Leger::get_x()
-{
-	return this->x;
-}
+double Leger::get_x() { return this->x; }
