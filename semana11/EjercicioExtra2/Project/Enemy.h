dd7ecf3c0f74c201f5a021d57d91b951c8812039
Abrancito <103963261+AbrahamAyquipa@users.noreply.github.com>
2022-10-31 18:27:09 -0500

    Update Enemy.h

--- semana11/EjercicioExtra2/Project/Enemy.h
+++ semana11/EjercicioExtra2/Project/Enemy.h
@@ -1,56 +1,66 @@
 #pragma once
+#include <iostream>
 using namespace System::Drawing;
 
 class Enemy {
-private:
-	int x, y; //ubicacion
-	int dx, dy; //movimiento
-	int idX, idY; // index
+private: 
+	int x, y;
+	int dx, dy;
+	int idX, idY;
 	int width, height;
-	int direction;
+	int direccion;
+	bool visibility;
 public:
-	Enemy(int width, int height, int direction) {
-		this->direction = direction;
-		this->width = width;
-		this->height = height;
+	Enemy(int w, int h, int d) {
+		width = w;
+		height = h;
+		direccion = d;
 		dx = dy = 20;
-		x = 400;
-		y = 200;
-		idX = idY = 0;
+		x = rand() % 900;
+		y = rand() % 200;
+		idX= idY = 0;
+		visibility = true;
 	}
 
 	~Enemy() {}
 
 	void draw(Graphics^ g, Bitmap^ bmp) {
-		Rectangle sectionShown = Rectangle(idX * width, idY * height, width, height);
-		Rectangle zoom = Rectangle(x, y, width * 1.0, height * 1.0);
-		g->DrawImage(bmp, zoom, sectionShown, GraphicsUnit::Pixel);
+		Rectangle sectionShow = Rectangle(idX * width, idY * height, width, height);
+		Rectangle zoom = Rectangle(x, y, width , height );
+		g->DrawImage(bmp, zoom, sectionShow, GraphicsUnit::Pixel);
 	}
 
 	void move(Graphics^ g) {
-
-		if (direction == 1) {
-			//limite del formulario
+		//horizontal
+		if (direccion == 1) { // los enemigos se mueven de izquierda a derecha
 			if (x + width * 1.0 > g->VisibleClipBounds.Width || x < 0) dx *= -1;
-
-			if (dx > 0) {
+			if (dx > 0) { //dx=20
 				idY = 2;
 			}
-			else idY = 1;
+			else // dx=-20
+				idY = 1;
 			x += dx;
 		}
+		//vertical
 		else {
-			if (y + height * 1.0 > g->VisibleClipBounds.Height || y < 0) dy *= -1;
-
+			if(y + height * 1.0 > g->VisibleClipBounds.Height || y < 0) dy *= -1;
 			if (dy > 0) {
 				idY = 0;
 			}
-			else idY = 3;
-
+			else
+				idY = 3;
 			y += dy;
-		}
 
+		}
 		idX++;
-		if (idX > 3)idX = 0;
+		if(idX > 3) 
+			idX = 0;
 	}
+
+	Rectangle getRectangle() {
+		return Rectangle(x, y, width * 1.0, height * 1.0);
+	}
+
+	void setVisibility(bool v) { visibility = v; }
+	bool getVisibility() { return visibility; }
 };
